fb1f90529ae9d4d420467b9027c915f6a45ec09a
Idarab <idarabrahamsson@gmail.com>
2017-03-22 12:53:27 +0100

    Add files via upload

--- findColor.m
+++ findColor.m
@@ -2,21 +2,43 @@ function findColor(image)
 
 resizeIm = 0.1;
 
-image = ('green1.png');
+%image = ('double1.jpg');
 image = imread(image);
 image = imresize(image,resizeIm);
 [m,n] = size(image);
 
 red = image(:,:,1);
 blue = image(:,:,3);
-green = image - red - blue;
+green = image - red;
 greenIntensity = rgb2gray(green);
 greenBinary = imbinarize(greenIntensity);
 s = regionprops(greenBinary,'centroid');
-s.Centroid
+centroids = cat(1, s.Centroid);
+a = regionprops(greenBinary, 'area');
+area = cat(1, a.Area);
 
 imshow(greenBinary);
-%viscircles(s.Centroid, 10);
+hold on
+plot(centroids(:,1),centroids(:,2), 'b*')
+hold off
+
+
+[sortedValues,sortIndex] = sort(area(:),'descend');
+
+if size(area,1) == 2
+    frontPos = centroids(sortIndex(1),:)
+    backPos = centroids(sortIndex(2),:)
+elseif size(area,1)== 3
+    destination = centroids(sortIndex(1),:)
+    frontPos = centroids(sortIndex(2),:)
+    backPos = centroids(sortIndex(3),:)
+else
+    error('did not find correct number of green spots');
+end
+    
+
+
+
 %[i,j]=find(greenBinary == 1)
 
 end
