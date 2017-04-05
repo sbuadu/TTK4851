7cab831ef70b1af5f7c6f29e92924a7ee8aeab73
Ida R Abrahamsson <ida.reiab@gmail.com>
2017-04-05 13:40:26 +0200

    now main as startfile for arduino

---
+++ main/drive.ino
@@ -0,0 +1,16 @@
+void drive(){
+  int time = 0; 
+  if( Serial.available() >0){       // if data is available to read
+    angle = Serial.read();          // read it and store it in 'state'
+
+    if (angle <= 180){
+      //right turn 
+      time = 5.109266313*10^(-3)*angle^2 + 63.03130035*angle^(1/2) - 182.2983324; 
+      turnRight(time); 
+      
+    }else{
+     //left turn 
+    time = 6.520934182*10^(-3)*angle^2 + 53.90658398*angle^(1/2) - 124.6841231; 
+    turnLeft(time); 
+  }
+}
